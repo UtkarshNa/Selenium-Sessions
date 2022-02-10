521567700c4cea7d7e531b812ac0c4f1611fc99d
UtkarshNa <86069871+UtkarshNa@users.noreply.github.com>
2022-02-11 02:21:55 +0530

    Update LocatorConcept.java

--- LocatorConcept.java
+++ LocatorConcept.java
@@ -7,8 +7,8 @@ public class LocatorConcept {
 	System.setProperty("webdriver.gecko.driver","C:\\geckodriver-v0.30.0-win64\\geckodriver.exe");
 	WebDriver driver=new FirefoxDriver();
 	
-	//System.setProperty("webdriver.chrome.driver", "C:\\chromedriver_win32\\chromedriver.exe");
-	//WebDriver driver=new ChromeDriver();
+	System.setProperty("webdriver.chrome.driver", "C:\\chromedriver_win32\\chromedriver.exe");
+	WebDriver driver=new ChromeDriver();
 	
 	
 	
